b1978a4763ec01bb3ed7ac94471d4f82f43c150e
bhaveshv2 <bhaveshv2@users.noreply.github.com>
2020-04-20 18:24:36 +0530

    Commit: converted the code into async await, create flash messages

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,42 +1,46 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
 
-module.exports.create = function(req,res){
+module.exports.create = async function(req,res){
     //find the post whether exist or not as some suspect can alter the website and then create the comment after that along with the insert the ids of the comment to the post 
-    Post.findById(req.body.post,function(err,post){
+    try{
+        let post =await Post.findById(req.body.post);
         if(post){
-            Comment.create({
+            let comment = await Comment.create({
                 content:req.body.content,
                 post:req.body.post,
                 user:req.user._id,
-            },function(err,comment){
-                if(err){
-                    console.log('Error in creating the comment');
-                    return;
-                }
+            });
+                    
                 //adding comment to post in comment id array after creating the comment
                 post.comments.push(comment);
                 //whenever we updating something in the db then we have to call save() after that to block it. 
                 post.save();
 
                 res.redirect('/');
-            });
         }
-    });
+    }catch(err){
+        console.log('Error',err);
+        return;
+    }
 }
 
-module.exports.destroy = function(req,res){
-    Comment.findById(req.params.id,function(err,comment){
+module.exports.destroy = async function(req,res){
+    try{
+        let comment = await Comment.findById(req.params.id);
         if(comment.user == req.user.id){
             let postId = comment.post;
             comment.remove();
 
-            Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}},function(err,post){
-                return res.redirect('back');
-            });
+            let post = await Post.findByIdAndUpdate(postId , {$pull:{comments:req.params.id}});
+            return res.redirect('back');
         }
         else{
             return res.redirect('back');
         }
-    });
+    }catch(err){
+        console.log('Error',err);
+        return; 
+    }
+    
 }
